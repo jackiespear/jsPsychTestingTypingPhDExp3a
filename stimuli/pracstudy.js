878efdc558a63319237c8b0b63dcc67155291388
Jackie Spear <jackiespear44@gmail.com>
2024-01-12 10:37:08 -0600

    updating practice stimuli and study block randomization

---
+++ stimuli/pracstudy.js
@@ -0,0 +1,18 @@
+pracstudy = [
+    {"stimulus":"<p style='font-size: 48px;'>BRICK - T_ _ _ _?</p>","data":{"type":"old","category":"produce","word":"TRICK"}},
+    {"stimulus":"<p style='font-size: 48px;'>FAT - B_ _?</p>","data":{"type":"old","category":"produce","word":"BAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>CAT - H_ _?</p>","data":{"type":"old","category":"produce","word":"HAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>POWER - T_ _ _ _?</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
+    {"stimulus":"<p style='font-size: 48px;'>MAT - R_ _?</p>","data":{"type":"old","category":"produce","word":"RAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>TIN - P_ _?</p>","data":{"type":"old","category":"produce","word":"PIN"}},
+    {"stimulus":"<p style='font-size: 48px;'>PEA - T_ _?</p>","data":{"type":"old","category":"produce","word":"TEA"}},
+    {"stimulus":"<p style='font-size: 48px;'>TEN - P_ _?</p>","data":{"type":"old","category":"produce","word":"PEN"}},
+    {"stimulus":"<p style='font-size: 48px;'>BAD - MAD</p>","data":{"type":"old","category":"read","word":"MAD"}},
+    {"stimulus":"<p style='font-size: 48px;'>LOG - DOG</p>","data":{"type":"old","category":"read","word":"DOG"}},
+    {"stimulus":"<p style='font-size: 48px;'>FAN - MAN</p>","data":{"type":"old","category":"read","word":"MAN"}},
+    {"stimulus":"<p style='font-size: 48px;'>SHEER - CHEER</p>","data":{"type":"old","category":"read","word":"CHEER"}},
+    {"stimulus":"<p style='font-size: 48px;'>GRAVE - BRAVE</p>","data":{"type":"old","category":"read","word":"BRAVE"}},
+    {"stimulus":"<p style='font-size: 48px;'>MONEY - HONEY</p>","data":{"type":"old","category":"read","word":"HONEY"}},
+    {"stimulus":"<p style='font-size: 48px;'>BOW - WOW</p>","data":{"type":"old","category":"read","word":"WOW"}},
+    {"stimulus":"<p style='font-size: 48px;'>ROT - LOT</p>","data":{"type":"old","category":"read","word":"LOT"}},
+]

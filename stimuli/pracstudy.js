2827b5d9427e88afbf467382a7a83cbece3922e0
jackiespear <jackiespear44@gmail.com>
2024-03-11 15:27:40 -0500

    updating practice procedure

--- stimuli/pracstudy.js
+++ stimuli/pracstudy.js
@@ -1,18 +1,50 @@
-pracstudy = [
-    {"stimulus":"<p style='font-size: 48px;'>BRICK - T_ _ _ _?</p>","data":{"type":"old","category":"produce","word":"TRICK"}},
-    {"stimulus":"<p style='font-size: 48px;'>FAT - B_ _?</p>","data":{"type":"old","category":"produce","word":"BAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>CAT - H_ _?</p>","data":{"type":"old","category":"produce","word":"HAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>POWER - T_ _ _ _?</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
-    {"stimulus":"<p style='font-size: 48px;'>MAT - R_ _?</p>","data":{"type":"old","category":"produce","word":"RAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>TIN - P_ _?</p>","data":{"type":"old","category":"produce","word":"PIN"}},
-    {"stimulus":"<p style='font-size: 48px;'>PEA - T_ _?</p>","data":{"type":"old","category":"produce","word":"TEA"}},
-    {"stimulus":"<p style='font-size: 48px;'>TEN - P_ _?</p>","data":{"type":"old","category":"produce","word":"PEN"}},
+pracstudy_block1 = [
+    {"stimulus":"<p style='font-size: 48px;'>BRICK - TRICK</p>","data":{"type":"old","category":"produce","word":"TRICK"}},
+    {"stimulus":"<p style='font-size: 48px;'>FAT - BAT</p>","data":{"type":"old","category":"produce","word":"BAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>CAT - HAT</p>","data":{"type":"old","category":"produce","word":"HAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>POWER - TOWER</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
+];
+pracstudy_block2 = [
+    {"stimulus":"<p style='font-size: 48px;'>MAT - RAT</p>","data":{"type":"old","category":"produce","word":"RAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>TIN - PIN</p>","data":{"type":"old","category":"produce","word":"PIN"}},
+    {"stimulus":"<p style='font-size: 48px;'>PEA - TEA</p>","data":{"type":"old","category":"produce","word":"TEA"}},
+    {"stimulus":"<p style='font-size: 48px;'>TEN - PEN</p>","data":{"type":"old","category":"produce","word":"PEN"}},
+];
+pracstudy_block3 = [
     {"stimulus":"<p style='font-size: 48px;'>BAD - MAD</p>","data":{"type":"old","category":"read","word":"MAD"}},
     {"stimulus":"<p style='font-size: 48px;'>LOG - DOG</p>","data":{"type":"old","category":"read","word":"DOG"}},
     {"stimulus":"<p style='font-size: 48px;'>FAN - MAN</p>","data":{"type":"old","category":"read","word":"MAN"}},
     {"stimulus":"<p style='font-size: 48px;'>SHEER - CHEER</p>","data":{"type":"old","category":"read","word":"CHEER"}},
+];
+pracstudy_block4 = [
     {"stimulus":"<p style='font-size: 48px;'>GRAVE - BRAVE</p>","data":{"type":"old","category":"read","word":"BRAVE"}},
     {"stimulus":"<p style='font-size: 48px;'>MONEY - HONEY</p>","data":{"type":"old","category":"read","word":"HONEY"}},
     {"stimulus":"<p style='font-size: 48px;'>BOW - WOW</p>","data":{"type":"old","category":"read","word":"WOW"}},
     {"stimulus":"<p style='font-size: 48px;'>ROT - LOT</p>","data":{"type":"old","category":"read","word":"LOT"}},
-]
+];
+
+practest_block1 = [
+    {"stimulus":"<p style='font-size: 48px;'>BRICK - ?</p>","data":{"type":"old","category":"produce","word":"TRICK"}},
+    {"stimulus":"<p style='font-size: 48px;'>FAT - ?</p>","data":{"type":"old","category":"produce","word":"BAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>CAT - ?</p>","data":{"type":"old","category":"produce","word":"HAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>POWER - ?</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
+];
+practest_block2 = [
+    {"stimulus":"<p style='font-size: 48px;'>MAT - ?</p>","data":{"type":"old","category":"produce","word":"RAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>TIN - ?</p>","data":{"type":"old","category":"produce","word":"PIN"}},
+    {"stimulus":"<p style='font-size: 48px;'>PEA - ?</p>","data":{"type":"old","category":"produce","word":"TEA"}},
+    {"stimulus":"<p style='font-size: 48px;'>TEN - ?</p>","data":{"type":"old","category":"produce","word":"PEN"}},
+];
+practest_block3 = [
+    {"stimulus":"<p style='font-size: 48px;'>LOG - ?</p>","data":{"type":"old","category":"read","word":"DOG"}},
+    {"stimulus":"<p style='font-size: 48px;'>FAN - ?</p>","data":{"type":"old","category":"read","word":"MAN"}},
+    {"stimulus":"<p style='font-size: 48px;'>BAD - ?</p>","data":{"type":"old","category":"read","word":"MAD"}},
+    {"stimulus":"<p style='font-size: 48px;'>SHEER - ?</p>","data":{"type":"old","category":"read","word":"CHEER"}},
+];
+practest_block4 = [
+    {"stimulus":"<p style='font-size: 48px;'>GRAVE - ?</p>","data":{"type":"old","category":"read","word":"BRAVE"}},
+    {"stimulus":"<p style='font-size: 48px;'>MONEY - ?</p>","data":{"type":"old","category":"read","word":"HONEY"}},
+    {"stimulus":"<p style='font-size: 48px;'>BOW - ?</p>","data":{"type":"old","category":"read","word":"WOW"}},
+    {"stimulus":"<p style='font-size: 48px;'>ROT - ?</p>","data":{"type":"old","category":"read","word":"LOT"}},
+];
+

73c2076fd7a8aa9ecfc6ab4a7d90cc65b2598f86
jackiespear <jackiespear44@gmail.com>
2024-03-26 15:48:25 -0500

    Cleaning things up
    small edits to the practice phase as well

--- stimuli/pracstudy.js
+++ stimuli/pracstudy.js
@@ -1,50 +1,50 @@
 pracstudy_block1 = [
-    {"stimulus":"<p style='font-size: 48px;'>BRICK - TRICK</p>","data":{"type":"old","category":"produce","word":"TRICK"}},
-    {"stimulus":"<p style='font-size: 48px;'>FAT - BAT</p>","data":{"type":"old","category":"produce","word":"BAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>CAT - HAT</p>","data":{"type":"old","category":"produce","word":"HAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>POWER - TOWER</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
+    {"stimulus":"<p style='font-size: 48px;'>BRICK - TOWER</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
+    {"stimulus":"<p style='font-size: 48px;'>FAT - TRICK</p>","data":{"type":"old","category":"produce","word":"TRICK"}},
+    {"stimulus":"<p style='font-size: 48px;'>CAT - MAT</p>","data":{"type":"old","category":"produce","word":"MAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>POWER - BRICK</p>","data":{"type":"old","category":"produce","word":"BRICK"}},
 ];
 pracstudy_block2 = [
-    {"stimulus":"<p style='font-size: 48px;'>MAT - RAT</p>","data":{"type":"old","category":"produce","word":"RAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>TIN - PIN</p>","data":{"type":"old","category":"produce","word":"PIN"}},
-    {"stimulus":"<p style='font-size: 48px;'>PEA - TEA</p>","data":{"type":"old","category":"produce","word":"TEA"}},
-    {"stimulus":"<p style='font-size: 48px;'>TEN - PEN</p>","data":{"type":"old","category":"produce","word":"PEN"}},
+    {"stimulus":"<p style='font-size: 48px;'>MAT - PEN</p>","data":{"type":"old","category":"produce","word":"PEN"}},
+    {"stimulus":"<p style='font-size: 48px;'>TIN - RAT</p>","data":{"type":"old","category":"produce","word":"RAT"}},
+    {"stimulus":"<p style='font-size: 48px;'>PEA - PIN</p>","data":{"type":"old","category":"produce","word":"PIN"}},
+    {"stimulus":"<p style='font-size: 48px;'>TEN - TEA</p>","data":{"type":"old","category":"produce","word":"TEA"}},
 ];
 pracstudy_block3 = [
-    {"stimulus":"<p style='font-size: 48px;'>BAD - MAD</p>","data":{"type":"old","category":"produce","word":"MAD"}},
-    {"stimulus":"<p style='font-size: 48px;'>LOG - DOG</p>","data":{"type":"old","category":"produce","word":"DOG"}},
-    {"stimulus":"<p style='font-size: 48px;'>FAN - MAN</p>","data":{"type":"old","category":"produce","word":"MAN"}},
-    {"stimulus":"<p style='font-size: 48px;'>SHEER - CHEER</p>","data":{"type":"old","category":"produce","word":"CHEER"}},
+    {"stimulus":"<p style='font-size: 48px;'>BAD - CHEER</p>","data":{"type":"old","category":"produce","word":"CHEER"}},
+    {"stimulus":"<p style='font-size: 48px;'>LOG - MAD</p>","data":{"type":"old","category":"produce","word":"MAD"}},
+    {"stimulus":"<p style='font-size: 48px;'>FAN - DOG</p>","data":{"type":"old","category":"produce","word":"DOG"}},
+    {"stimulus":"<p style='font-size: 48px;'>SHEER - MAD</p>","data":{"type":"old","category":"produce","word":"MAD"}},
 ];
 pracstudy_block4 = [
-    {"stimulus":"<p style='font-size: 48px;'>GRAVE - BRAVE</p>","data":{"type":"old","category":"produce","word":"BRAVE"}},
-    {"stimulus":"<p style='font-size: 48px;'>MONEY - HONEY</p>","data":{"type":"old","category":"produce","word":"HONEY"}},
-    {"stimulus":"<p style='font-size: 48px;'>BOW - WOW</p>","data":{"type":"old","category":"produce","word":"WOW"}},
-    {"stimulus":"<p style='font-size: 48px;'>ROT - LOT</p>","data":{"type":"old","category":"produce","word":"LOT"}},
+    {"stimulus":"<p style='font-size: 48px;'>GRAVE - LOT</p>","data":{"type":"old","category":"produce","word":"LOT"}},
+    {"stimulus":"<p style='font-size: 48px;'>MONEY - BRAVE</p>","data":{"type":"old","category":"produce","word":"BRAVE"}},
+    {"stimulus":"<p style='font-size: 48px;'>BOW - HONEY</p>","data":{"type":"old","category":"produce","word":"HONEY"}},
+    {"stimulus":"<p style='font-size: 48px;'>ROT - WOW</p>","data":{"type":"old","category":"produce","word":"WOW"}},
 ];
 
 practest_block1 = [
-    {"stimulus":"<p style='font-size: 48px;'>BRICK - ?</p>","data":{"type":"old","category":"produce","word":"TRICK"}},
-    {"stimulus":"<p style='font-size: 48px;'>FAT - ?</p>","data":{"type":"old","category":"produce","word":"BAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>CAT - ?</p>","data":{"type":"old","category":"produce","word":"HAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>POWER - ?</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
+    {"stimulus":"<p style='font-size: 48px;'>BRICK - ?</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
+    {"stimulus":"<p style='font-size: 48px;'>FAT - ?</p>","data":{"type":"old","category":"produce","word":"TRICK"}},
+    //{"stimulus":"<p style='font-size: 48px;'>CAT - ?</p>","data":{"type":"old","category":"produce","word":"HAT"}},
+    //{"stimulus":"<p style='font-size: 48px;'>POWER - ?</p>","data":{"type":"old","category":"produce","word":"TOWER"}},
 ];
 practest_block2 = [
-    {"stimulus":"<p style='font-size: 48px;'>MAT - ?</p>","data":{"type":"old","category":"produce","word":"RAT"}},
-    {"stimulus":"<p style='font-size: 48px;'>TIN - ?</p>","data":{"type":"old","category":"produce","word":"PIN"}},
-    {"stimulus":"<p style='font-size: 48px;'>PEA - ?</p>","data":{"type":"old","category":"produce","word":"TEA"}},
-    {"stimulus":"<p style='font-size: 48px;'>TEN - ?</p>","data":{"type":"old","category":"produce","word":"PEN"}},
+    //{"stimulus":"<p style='font-size: 48px;'>MAT - ?</p>","data":{"type":"old","category":"produce","word":"RAT"}},
+    //{"stimulus":"<p style='font-size: 48px;'>TIN - ?</p>","data":{"type":"old","category":"produce","word":"PIN"}},
+    {"stimulus":"<p style='font-size: 48px;'>PEA - ?</p>","data":{"type":"old","category":"produce","word":"PIN"}},
+    {"stimulus":"<p style='font-size: 48px;'>TEN - ?</p>","data":{"type":"old","category":"produce","word":"TEA"}},
 ];
 practest_block3 = [
-    {"stimulus":"<p style='font-size: 48px;'>LOG - ?</p>","data":{"type":"old","category":"produce","word":"DOG"}},
-    {"stimulus":"<p style='font-size: 48px;'>FAN - ?</p>","data":{"type":"old","category":"produce","word":"MAN"}},
-    {"stimulus":"<p style='font-size: 48px;'>BAD - ?</p>","data":{"type":"old","category":"produce","word":"MAD"}},
-    {"stimulus":"<p style='font-size: 48px;'>SHEER - ?</p>","data":{"type":"old","category":"produce","word":"CHEER"}},
+    {"stimulus":"<p style='font-size: 48px;'>LOG - ?</p>","data":{"type":"old","category":"produce","word":"MAD"}},
+    //{"stimulus":"<p style='font-size: 48px;'>FAN - ?</p>","data":{"type":"old","category":"produce","word":"MAN"}},
+    {"stimulus":"<p style='font-size: 48px;'>BAD - ?</p>","data":{"type":"old","category":"produce","word":"CHEER"}},
+    //{"stimulus":"<p style='font-size: 48px;'>SHEER - ?</p>","data":{"type":"old","category":"produce","word":"CHEER"}},
 ];
 practest_block4 = [
-    {"stimulus":"<p style='font-size: 48px;'>GRAVE - ?</p>","data":{"type":"old","category":"produce","word":"BRAVE"}},
-    {"stimulus":"<p style='font-size: 48px;'>MONEY - ?</p>","data":{"type":"old","category":"produce","word":"HONEY"}},
-    {"stimulus":"<p style='font-size: 48px;'>BOW - ?</p>","data":{"type":"old","category":"produce","word":"WOW"}},
-    {"stimulus":"<p style='font-size: 48px;'>ROT - ?</p>","data":{"type":"old","category":"produce","word":"LOT"}},
+    //{"stimulus":"<p style='font-size: 48px;'>GRAVE - ?</p>","data":{"type":"old","category":"produce","word":"BRAVE"}},
+    {"stimulus":"<p style='font-size: 48px;'>MONEY - ?</p>","data":{"type":"old","category":"produce","word":"BRAVE"}},
+    {"stimulus":"<p style='font-size: 48px;'>BOW - ?</p>","data":{"type":"old","category":"produce","word":"HONEY"}},
+    //{"stimulus":"<p style='font-size: 48px;'>ROT - ?</p>","data":{"type":"old","category":"produce","word":"LOT"}},
 ];
 
